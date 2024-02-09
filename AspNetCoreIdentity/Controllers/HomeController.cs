1eb07e889ce1ae507b8ab76a73b7c47faf594bce
mfglr <mfgglr@outlook.com>
2024-02-10 02:53:42 +0300

    created sing up page

--- AspNetCoreIdentity/Controllers/HomeController.cs
+++ AspNetCoreIdentity/Controllers/HomeController.cs
@@ -23,6 +23,12 @@ namespace AspNetCoreIdentity.Controllers
             return View();
         }
 
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ AspNetCoreIdentity/ViewModels/SingUpViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentity.ViewModels
+{
+    public class SingUpViewModel
+    {
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+       
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        
+        [Display(Name = "Phone")]
+        public string Phone {  get; set; }
+
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Password confirm")]
+        public string PasswordConfirm { get; set; }
+    }
+}
