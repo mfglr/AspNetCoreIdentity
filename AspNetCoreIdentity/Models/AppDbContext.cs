67517a5d2627bf9cbe0a17c44dc5ec14af182467
mfglr <mfgglr@outlook.com>
2024-02-10 02:12:29 +0300

    database and identity configurations

---
+++ AspNetCoreIdentity/Models/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace AspNetCoreIdentity.Models
+{
+    public class AppDbContext : IdentityDbContext<AppUser,AppRole,string>
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+    }
+
+
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer("Data Source=THENQLV;Initial Catalog=IdentityDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            return new AppDbContext(optionsBuilder.Options);
+
+        }
+    }
+
+}
--- AspNetCoreIdentity/Program.cs
+++ AspNetCoreIdentity/Program.cs
@@ -1,7 +1,16 @@
+using AspNetCoreIdentity.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services
+    .AddDbContext<AppDbContext>(
+        options => options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"))
+    )
+    .AddIdentity<AppUser,AppRole>()
+    .AddEntityFrameworkStores<AppDbContext>();
 
 var app = builder.Build();
 
