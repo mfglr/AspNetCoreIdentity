e31f02b0fc934fc1225383ec89cf0b482dc583e4
mfglr <mfgglr@outlook.com>
2024-02-11 19:48:53 +0300

    User page

--- AspNetCoreIdentity/Controllers/MemberController.cs
+++ AspNetCoreIdentity/Controllers/MemberController.cs
@@ -1,4 +1,5 @@
 ﻿using AspNetCoreIdentity.Models;
+using AspNetCoreIdentity.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -10,15 +11,28 @@ namespace AspNetCoreIdentity.Controllers
     public class MemberController : Controller
     {
         private readonly SignInManager<AppUser> _signManager;
+        private readonly UserManager<AppUser> _userManager;
 
-        public MemberController(SignInManager<AppUser> signManager)
+        public MemberController(SignInManager<AppUser> signManager, UserManager<AppUser> userManager)
         {
             _signManager = signManager;
+            _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var user = await _userManager.FindByNameAsync(User.Identity!.Name!);
+
+            if (user == null) throw new Exception("User not found!");
+
+            var userViewModel = new UserViewModel()
+            {
+                Email = user.Email!,
+                UserName = user.UserName,
+                Phone = user.PhoneNumber
+            };
+
+            return View(userViewModel);
         } 
 
         public async Task<IActionResult> Logout()
---
+++ AspNetCoreIdentity/ViewModels/UserViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AspNetCoreIdentity.ViewModels
+{
+    public class UserViewModel
+    {
+        public string UserName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? Phone {  get; set; }
+    }
+}
