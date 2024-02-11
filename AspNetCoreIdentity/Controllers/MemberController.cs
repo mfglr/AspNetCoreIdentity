7bab6dd50523d20a1e298a9682918e01949da3e2
mfglr <mfgglr@outlook.com>
2024-02-11 20:46:49 +0300

    change password page

--- AspNetCoreIdentity/Controllers/MemberController.cs
+++ AspNetCoreIdentity/Controllers/MemberController.cs
@@ -3,6 +3,8 @@ using AspNetCoreIdentity.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace AspNetCoreIdentity.Controllers
 {
@@ -12,11 +14,14 @@ namespace AspNetCoreIdentity.Controllers
     {
         private readonly SignInManager<AppUser> _signManager;
         private readonly UserManager<AppUser> _userManager;
+        private readonly AppDbContext _context;
 
-        public MemberController(SignInManager<AppUser> signManager, UserManager<AppUser> userManager)
+
+        public MemberController(SignInManager<AppUser> signManager, UserManager<AppUser> userManager, AppDbContext context)
         {
             _signManager = signManager;
             _userManager = userManager;
+            _context = context;
         }
 
         public async Task<IActionResult> Index()
@@ -34,6 +39,54 @@ namespace AspNetCoreIdentity.Controllers
 
             return View(userViewModel);
         } 
+
+        public IActionResult PasswordChange()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async  Task<IActionResult> PasswordChange(PasswordChangeViewModel request)
+        {
+            if (!ModelState.IsValid) return View();
+
+
+            var user = await _userManager.FindByNameAsync(User.Identity!.Name!);
+            if (user == null) throw new Exception("User not found");
+
+            var IsMatchedPassword = await _userManager.CheckPasswordAsync(user, request.OldPassword);
+
+            if (!IsMatchedPassword)
+            {
+                ModelState.AddModelError(string.Empty, "Not match password");
+                return View();
+            }
+
+            var result = await _userManager.ChangePasswordAsync(user,request.OldPassword,request.NewPassword);
+
+            if (!result.Succeeded) {
+                foreach (var error in result.Errors)
+                    ModelState.AddModelError(string.Empty, error.Description);
+                return View();
+            }
+
+            result = await _userManager.UpdateSecurityStampAsync(user);
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                    ModelState.AddModelError(string.Empty, error.Description);
+                return View();
+            }
+
+            await _signManager.SignOutAsync();
+            await _signManager.PasswordSignInAsync(user, request.NewPassword, true, false);
+
+
+
+            return RedirectToAction("signin", "home");
+        }
+
+
 
         public async Task<IActionResult> Logout()
         {
---
+++ AspNetCoreIdentity/ViewModels/PasswordChangeViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentity.ViewModels
+{
+    public class PasswordChangeViewModel
+    {
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Old Password")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
+        [MinLength(9)]
+        public string NewPassword { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(NewPassword))]
+        [Display(Name = "Confirm Password")]
+        [MinLength(9)]
+        public string ConfirmPassword { get; set;}
+    }
+}
